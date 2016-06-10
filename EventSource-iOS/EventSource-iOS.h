9285c980b806c1c3a7e15a578664d0800ff5b18d
Neil Cowburn <neilco@gmail.com>
2016-06-10 18:28:10 +0100

    Add iOS framework target

---
+++ EventSource-iOS/EventSource-iOS.h
@@ -0,0 +1,19 @@
+//
+//  EventSource-iOS.h
+//  EventSource-iOS
+//
+//  Created by Neil on 10/06/2016.
+//  Copyright © 2016 Neil Cowburn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EventSource-iOS.
+FOUNDATION_EXPORT double EventSource_iOSVersionNumber;
+
+//! Project version string for EventSource-iOS.
+FOUNDATION_EXPORT const unsigned char EventSource_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventSource_iOS/PublicHeader.h>
+
+
