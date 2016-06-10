5f40f88499205f6e77b84ed3c890db3aeb3aea15
Neil Cowburn <neilco@gmail.com>
2016-06-10 18:31:45 +0100

    Add Mac framework target

---
+++ EventSource-Mac/EventSource-Mac.h
@@ -0,0 +1,19 @@
+//
+//  EventSource-Mac.h
+//  EventSource-Mac
+//
+//  Created by Neil on 10/06/2016.
+//  Copyright © 2016 Neil Cowburn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EventSource-Mac.
+FOUNDATION_EXPORT double EventSource_MacVersionNumber;
+
+//! Project version string for EventSource-Mac.
+FOUNDATION_EXPORT const unsigned char EventSource_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventSource_Mac/PublicHeader.h>
+
+
